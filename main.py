59db7894c6837169f5f8d1fc543f66c355615eb9
Jenny Yu <jennyyu73@gmail.com>
2020-04-12 15:48:25 -0400

    Created UI for login screen and register screen

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ def init(data):
     data.registerButton = Button(data.width/2, data.height*0.7, 100, 30, "Register")
     data.creditButton = Button(data.width*0.9, data.height*0.9, 70, 25, "Credit")
     data.creditBackButton = Button(data.width/2, data.height*0.85, 80, 25, "< Back")
+    data.submitButton = Button(data.width/2, data.height*0.8, 80, 25, "Submit")
+    data.underlineBackButton = UnderlineButton(data.width/2, data.height*0.9,
+        30, 20, "Back")
+    data.input = False
 
 ################################################################################
 # general helpers/classes
@@ -35,7 +39,9 @@ def getFont(fontSize):
     return myFont
 
 def roundRectangle(canvas, x1, y1, x2, y2, r=25, **kwargs):    
-    points = (x1+r, y1, x1+r, y1, x2-r, y1, x2-r, y1, x2, y1, x2, y1+r, x2, y1+r, x2, y2-r, x2, y2-r, x2, y2, x2-r, y2, x2-r, y2, x1+r, y2, x1+r, y2, x1, y2, x1, y2-r, x1, y2-r, x1, y1+r, x1, y1+r, x1, y1)
+    points = (x1+r, y1, x1+r, y1, x2-r, y1, x2-r, y1, x2, y1, x2, y1+r, x2, y1+r, 
+        x2, y2-r, x2, y2-r, x2, y2, x2-r, y2, x2-r, y2, x1+r, y2, x1+r, y2, x1, 
+        y2, x1, y2-r, x1, y2-r, x1, y1+r, x1, y1+r, x1, y1)
     return canvas.create_polygon(points, **kwargs, smooth=True)
 
 #exit canvas
@@ -66,9 +72,15 @@ class Button(object):
             width=2, outline="black")
         canvas.create_text(self.x, self.y, text=self.text, font=getFont(fontSize))
 
-    def onPress(self, data, x, y):
+    def onPress(self, x, y):
         if (self.x0 < x < self.x1) and (self.y0 < y < self.y1):
             return True
+
+class UnderlineButton(Button):
+    def draw(self, canvas, fontSize):
+        canvas.create_line(self.x0, self.y1, self.x1, self.y1, width=2, 
+            fill="black")
+        canvas.create_text(self.x, self.y, text=self.text, font=getFont(fontSize))
 
 
 ################################################################################
@@ -76,11 +88,11 @@ class Button(object):
 ################################################################################
 
 def startMousePressed(event, data):
-    if data.loginButton.onPress(data, event.x, event.y):
+    if data.loginButton.onPress(event.x, event.y):
         data.mode = "login"
-    elif data.registerButton.onPress(data, event.x, event.y):
+    elif data.registerButton.onPress(event.x, event.y):
         data.mode = "register"
-    elif data.creditButton.onPress(data, event.x, event.y):
+    elif data.creditButton.onPress(event.x, event.y):
         data.mode = "credit"
 
 def startKeyPressed(event, data):
@@ -102,7 +114,7 @@ def startRedrawAll(canvas, data):
 ################################################################################
 
 def creditMousePressed(event, data):
-    if data.creditBackButton.onPress(data, event.x, event.y):
+    if data.creditBackButton.onPress(event.x, event.y):
         data.mode = "start"
 
 def creditKeyPressed(event, data):
@@ -128,6 +140,64 @@ def creditRedrawAll(canvas, data):
         font=getFont(25), anchor=NW)
 
 ################################################################################
+# register mode
+################################################################################
+
+def registerMousePressed(event, data):
+    if data.underlineBackButton.onPress(event.x, event.y):
+        data.mode = "start"
+
+def registerKeyPressed(event, data):
+    pass
+
+def registerTimerFired(data):
+    pass
+
+def registerRedrawAll(canvas, data):
+    drawIndexCard(canvas, data)
+    canvas.create_text(data.width/2, data.height*0.1, text="Register as new user",
+        font=getFont(35))
+    data.submitButton.draw(canvas, 25)
+    data.underlineBackButton.draw(canvas, 20)
+    canvas.create_text(data.width*0.3, data.height*0.4, text="username:",
+        font=getFont(30))
+    canvas.create_text(data.width*0.3, data.height*0.54, text="password:",
+        font=getFont(30))
+    roundRectangle(canvas, data.width*0.42, data.height*0.35, data.width*0.7,
+        data.height*0.45, width=2, outline="black", fill="white")
+    roundRectangle(canvas, data.width*0.42, data.height*0.49, data.width*0.7,
+        data.height*0.59, width=2, outline="black", fill="white")
+
+################################################################################
+# login mode
+################################################################################
+
+def loginMousePressed(event, data):
+    if data.underlineBackButton.onPress(event.x, event.y):
+        data.mode = "start"
+
+def loginKeyPressed(event, data):
+    pass
+
+def loginTimerFired(data):
+    pass
+
+def loginRedrawAll(canvas, data):
+    drawIndexCard(canvas, data)
+    canvas.create_text(data.width/2, data.height*0.1, text="Login as existing user",
+        font=getFont(35))
+    data.submitButton.draw(canvas, 25)
+    data.underlineBackButton.draw(canvas, 20)
+    canvas.create_text(data.width*0.3, data.height*0.4, text="username:",
+        font=getFont(30))
+    canvas.create_text(data.width*0.3, data.height*0.54, text="password:",
+        font=getFont(30))
+    roundRectangle(canvas, data.width*0.42, data.height*0.35, data.width*0.7,
+        data.height*0.45, width=2, outline="black", fill="white")
+    roundRectangle(canvas, data.width*0.42, data.height*0.49, data.width*0.7,
+        data.height*0.59, width=2, outline="black", fill="white")
+
+################################################################################
 # mode toggle
 ################################################################################
 
@@ -136,24 +206,40 @@ def mousePressed(event, data):
         startMousePressed(event, data)
     elif data.mode == "credit":
         creditMousePressed(event, data)
+    elif data.mode == "register":
+        registerMousePressed(event, data)
+    elif data.mode == "login":
+        loginMousePressed(event, data)
 
 def keyPressed(event, data):
     if data.mode == "start":
         startKeyPressed(event, data)
     elif data.mode == "credit":
         creditKeyPressed(event, data)
+    elif data.mode == "register":
+        registerKeyPressed(event, data)
+    elif data.mode == "login":
+        loginKeyPressed(event, data)
 
 def timerFired(data):
     if data.mode == "start":
         startTimerFired(data)
     elif data.mode == "credit":
         creditTimerFired(data)
+    elif data.mode == "register":
+        registerTimerFired(data)
+    elif data.mode == "login":
+        loginTimerFired(data)
 
 def redrawAll(canvas, data):
     if data.mode == "start":
         startRedrawAll(canvas, data)
     elif data.mode == "credit":
         creditRedrawAll(canvas, data)
+    elif data.mode == "register":
+        registerRedrawAll(canvas, data)
+    elif data.mode == "login":
+        loginRedrawAll(canvas, data)
 
 
 ################################################################################
